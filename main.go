6fac9714e7bcb05b52c3ab0f001f2673dc1dd90e
Amin Busaboul <amin.busaboul@unzer.com>
2023-06-23 17:04:02 +0200

    feat: load aws credentials from environment variables if no profile is provided

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"strings"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/inspector2/types"
 	"github.com/rootameen/vulpine/pkg/ecr"
@@ -24,17 +25,26 @@ func main() {
 	scanTarget := flag.String("scanTarget", "ecr", "scanTarget: eks (to show findings for pods in eks), ecr (to show findings for all images in ECR)")
 	scanType := flag.String("scanType", "short", "scanType type: short (100 findings), full")
 	ecrImageRegistry := flag.String("ecrImageRegistry", "", "ECR Image Registry to scan, e.g. 424851304182")
-	ecrProfile := flag.String("ecrProfile", "infra", "AWS Profile to use which contains ECR Repos")
+	ecrProfile := flag.String("ecrProfile", "", "AWS Profile to use which contains ECR Repos")
 
 	flag.Parse()
 
+	var cfg aws.Config
+	var err error
+
 	// ECR AWS Account Auth
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("eu-central-1"), config.WithSharedConfigProfile(*ecrProfile))
+	if *ecrProfile == "" {
+		cfg, err = config.LoadDefaultConfig(context.TODO(), config.WithRegion("eu-central-1"))
+		if err != nil {
+			log.Fatalf("Unable to load SDK config, %v", err)
+		}
 
-	if err != nil {
-		log.Fatalf("Unable to load SDK config, %v", err)
+	} else {
+		cfg, err = config.LoadDefaultConfig(context.TODO(), config.WithRegion("eu-central-1"), config.WithSharedConfigProfile(*ecrProfile))
+		if err != nil {
+			log.Fatalf("Unable to load SDK config, %v", err)
+		}
 	}
-
 	// generate list of ECR Repos and images
 	ecrRepos := ecr.GenerateEcrImageList(cfg)
 
