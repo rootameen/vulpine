bc08b7e23d12a5d444da3eaa42aaf90eea23dbec
Amin Busaboul <amin.busaboul@unzer.com>
2023-07-06 12:24:00 +0200

    fix: obtain results for container images when findings exceed 100

--- pkg/scan/scan.go
+++ pkg/scan/scan.go
@@ -1,6 +1,8 @@
 package scan
 
 import (
+	"fmt"
+
 	"github.com/aws/aws-sdk-go-v2/service/inspector2"
 	"github.com/aws/aws-sdk-go-v2/service/inspector2/types"
 	"github.com/rootameen/vulpine/pkg/ecr"
@@ -11,14 +13,13 @@ import (
 func ScanFindings(scanTarget *string, results []types.Finding, inspectorClient *inspector2.Client, scanType *string, ecrImageRegistry *string, k8sctx *string, ecrRepos []ecr.ECRRepo) []types.Finding {
 
 	if *scanTarget == "ecr" {
-
-		results = inspector.ListInspectorFindings(inspectorClient, results, scanType, ecrImageRegistry)
+		results = inspector.ListInspectorFindingsEcr(inspectorClient, results, scanType, ecrImageRegistry)
 	} else if *scanTarget == "eks" {
 		pods := eks.GenerateClusterPodList(k8sctx)
-
 		deployedImages := make(map[string]string)
 		eks.IsImageDeployed(ecrRepos, pods, deployedImages)
 		for repoName, imageTag := range deployedImages {
+			fmt.Println("Obtaining findings for running image: ", repoName, imageTag)
 			results = inspector.ListInspectorFindingsByRepoImage(inspectorClient, results, repoName, imageTag)
 		}
 	}
